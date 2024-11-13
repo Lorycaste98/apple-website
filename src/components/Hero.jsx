53e1118ba9b437d3bb7afc2607f6f156b927c625
Lorenzo <lorenzo.castelletti98@gmail.com>
2024-11-13 08:35:15 +0100

    prova

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
-          iPhone 15 Pro
+          iPhone 15 ProXXxXXX
         </p>
         <div className="md:w-10/12 w-9/12">
           <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
